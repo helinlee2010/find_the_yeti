f88bc4d70caac6d762ce805a86170491e2583ed8
Helin Li <helinlee2010@gmail.com>
2020-01-12 21:06:58 -0800

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+  //This code will run after your page loads
+  alert("Hello, guess where is yeti? ");
+  $(".yeti").mousedown(function(){
+    alert("Yaaarrrr! You got me!");
+  });
+});
